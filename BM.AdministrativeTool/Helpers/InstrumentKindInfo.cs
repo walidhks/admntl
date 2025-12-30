1b0d2cfb9d9bc9b6501b42dae4d09fe3a554ff39
Walid <walidzbenz@gmail.com>
2025-12-30 21:52:58 +0100

    added new instrument to instrument txt files

--- BM.AdministrativeTool/Helpers/InstrumentKindInfo.cs
+++ BM.AdministrativeTool/Helpers/InstrumentKindInfo.cs
@@ -2,88 +2,81 @@
 
 namespace BM.AdministrativeTool.Helpers
 {
-	// Token: 0x0200001C RID: 28
-	public class InstrumentKindInfo
-	{
-		// Token: 0x06000093 RID: 147 RVA: 0x000041D0 File Offset: 0x000023D0
-		public InstrumentKindInfo(string kindName, InstrumentKindInfo info)
-		{
-			this.AutomateKind = info.AutomateKind;
-			this.Automate = kindName;
-			this.Priority = info.Priority;
-			this.Code = info.Code;
-			this.InstrumentBaudRate = info.InstrumentBaudRate;
-			this.InstrumentDataBits = info.InstrumentDataBits;
-			this.InstrumentPortName = info.InstrumentPortName;
-			this.LastAnalysisId = info.LastAnalysisId;
-		}
+    public class InstrumentKindInfo
+    {
+        public InstrumentKindInfo(string kindName, InstrumentKindInfo info)
+        {
+            this.AutomateKind = info.AutomateKind;
+            this.Automate = kindName;
+            this.Priority = info.Priority;
+            this.Code = info.Code;
+            this.InstrumentBaudRate = info.InstrumentBaudRate;
+            this.InstrumentDataBits = info.InstrumentDataBits;
+            this.InstrumentPortName = info.InstrumentPortName;
+            this.LastAnalysisId = info.LastAnalysisId;
 
-		// Token: 0x06000094 RID: 148 RVA: 0x00004248 File Offset: 0x00002448
-		public InstrumentKindInfo(string s)
-		{
-			string[] array = s.Split(new char[]
-			{
-				','
-			});
-			this.AutomateKind = (this.Automate = array[0]);
-			this.Priority = array[1];
-			this.Code = array[2];
-			this.InstrumentBaudRate = array[3];
-			this.InstrumentDataBits = array[4];
-			this.InstrumentPortName = array[5];
-			this.LastAnalysisId = array[6];
-		}
+            this.Parity = info.Parity;
+            this.L1 = info.L1;
+            this.L2 = info.L2;
+            this.L3 = info.L3;
+            this.B1 = info.B1;
+            this.Mode = info.Mode;
+        }
 
-		// Token: 0x06000095 RID: 149 RVA: 0x00002558 File Offset: 0x00000758
-		public InstrumentKindInfo(string kindName, InstrumentKindInfo info, string c, string mode) : this(kindName, info)
-		{
-			this.InstrumentDataBits = c;
-			this.Mode = mode;
-		}
+        public InstrumentKindInfo(string s)
+        {
+            string[] array = s.Split(new char[]
+            {
+                ','
+            });
 
-		// Token: 0x1700001A RID: 26
-		// (get) Token: 0x06000096 RID: 150 RVA: 0x00002575 File Offset: 0x00000775
-		// (set) Token: 0x06000097 RID: 151 RVA: 0x0000257D File Offset: 0x0000077D
-		public string Mode { get; set; }
+            // Safety Check for standard fields
+            if (array.Length < 7)
+            {
+                // Initialize empty to prevent null ref later, or handle error
+                this.AutomateKind = this.Automate = "INVALID";
+                this.Priority = this.Code = this.InstrumentBaudRate = "";
+                return;
+            }
 
-		// Token: 0x1700001B RID: 27
-		// (get) Token: 0x06000098 RID: 152 RVA: 0x00002586 File Offset: 0x00000786
-		// (set) Token: 0x06000099 RID: 153 RVA: 0x0000258E File Offset: 0x0000078E
-		public string AutomateKind { get; set; }
+            this.AutomateKind = (this.Automate = array[0]);
+            this.Priority = array[1];
+            this.Code = array[2];
+            this.InstrumentBaudRate = array[3];
+            this.InstrumentDataBits = array[4];
+            this.InstrumentPortName = array[5];
+            this.LastAnalysisId = array[6];
 
-		// Token: 0x1700001C RID: 28
-		// (get) Token: 0x0600009A RID: 154 RVA: 0x00002597 File Offset: 0x00000797
-		// (set) Token: 0x0600009B RID: 155 RVA: 0x0000259F File Offset: 0x0000079F
-		public string Automate { get; set; }
+            // Extended Fields (Only read if they exist)
+            if (array.Length > 7) this.Parity = array[7];
+            if (array.Length > 8) this.L1 = array[8];
+            if (array.Length > 9) this.L2 = array[9];
+            if (array.Length > 10) this.L3 = array[10];
+            if (array.Length > 11) this.B1 = array[11];
+            if (array.Length > 12) this.Mode = array[12];
+        }
 
-		// Token: 0x1700001D RID: 29
-		// (get) Token: 0x0600009C RID: 156 RVA: 0x000025A8 File Offset: 0x000007A8
-		// (set) Token: 0x0600009D RID: 157 RVA: 0x000025B0 File Offset: 0x000007B0
-		public string Priority { get; set; }
+        public InstrumentKindInfo(string kindName, InstrumentKindInfo info, string c, string mode) : this(kindName, info)
+        {
+            this.InstrumentDataBits = c;
+            this.Mode = mode;
+        }
 
-		// Token: 0x1700001E RID: 30
-		// (get) Token: 0x0600009E RID: 158 RVA: 0x000025B9 File Offset: 0x000007B9
-		// (set) Token: 0x0600009F RID: 159 RVA: 0x000025C1 File Offset: 0x000007C1
-		public string Code { get; set; }
+        public string Mode { get; set; }
+        public string AutomateKind { get; set; }
+        public string Automate { get; set; }
+        public string Priority { get; set; }
+        public string Code { get; set; }
+        public string InstrumentBaudRate { get; set; }
+        public string InstrumentDataBits { get; set; }
+        public string InstrumentPortName { get; set; }
+        public string LastAnalysisId { get; set; }
 
-		// Token: 0x1700001F RID: 31
-		// (get) Token: 0x060000A0 RID: 160 RVA: 0x000025CA File Offset: 0x000007CA
-		// (set) Token: 0x060000A1 RID: 161 RVA: 0x000025D2 File Offset: 0x000007D2
-		public string InstrumentBaudRate { get; set; }
-
-		// Token: 0x17000020 RID: 32
-		// (get) Token: 0x060000A2 RID: 162 RVA: 0x000025DB File Offset: 0x000007DB
-		// (set) Token: 0x060000A3 RID: 163 RVA: 0x000025E3 File Offset: 0x000007E3
-		public string InstrumentDataBits { get; set; }
-
-		// Token: 0x17000021 RID: 33
-		// (get) Token: 0x060000A4 RID: 164 RVA: 0x000025EC File Offset: 0x000007EC
-		// (set) Token: 0x060000A5 RID: 165 RVA: 0x000025F4 File Offset: 0x000007F4
-		public string InstrumentPortName { get; set; }
-
-		// Token: 0x17000022 RID: 34
-		// (get) Token: 0x060000A6 RID: 166 RVA: 0x000025FD File Offset: 0x000007FD
-		// (set) Token: 0x060000A7 RID: 167 RVA: 0x00002605 File Offset: 0x00000805
-		public string LastAnalysisId { get; set; }
-	}
+        // New Properties
+        public string Parity { get; set; }
+        public string L1 { get; set; }
+        public string L2 { get; set; }
+        public string L3 { get; set; }
+        public string B1 { get; set; }
+    }
 }
--- BM.AdministrativeTool/Model/Instrument.cs
+++ BM.AdministrativeTool/Model/Instrument.cs
@@ -3,87 +3,28 @@ using System.IO.Ports;
 
 namespace BM.AdministrativeTool.Model
 {
-	// Token: 0x02000007 RID: 7
-	public class Instrument
-	{
-		// Token: 0x17000005 RID: 5
-		// (get) Token: 0x06000018 RID: 24 RVA: 0x00002131 File Offset: 0x00000331
-		// (set) Token: 0x06000019 RID: 25 RVA: 0x00002139 File Offset: 0x00000339
-		public int InstrumentId { get; set; }
+    public class Instrument
+    {
+        public int InstrumentId { get; set; }
+        public bool InstrumentUsed { get; set; }
+        public string InstrumentName { get; set; }
+        public string InstrumentDescription { get; set; }
+        public string InstrumentPortName { get; set; }
+        public string InstrumentBaudRate { get; set; }
+        public string InstrumentDataBits { get; set; }
+        public long LastAnalysisId { get; set; }
+        public StopBits? InstrumentStopBits { get; set; }
+        public Parity? InstrumentParity { get; set; }
+        public string InstrumentStd { get; set; }
+        public int? ServiceId { get; set; }
+        public int Mode { get; set; }
+        public long? LaboratoryUnitId { get; set; }
+        public int InstrumentCode { get; set; }
+        public string MiddlewareServiceName { get; set; }
 
-		// Token: 0x17000006 RID: 6
-		// (get) Token: 0x0600001A RID: 26 RVA: 0x00002142 File Offset: 0x00000342
-		// (set) Token: 0x0600001B RID: 27 RVA: 0x0000214A File Offset: 0x0000034A
-		public bool InstrumentUsed { get; set; }
-
-		// Token: 0x17000007 RID: 7
-		// (get) Token: 0x0600001C RID: 28 RVA: 0x00002153 File Offset: 0x00000353
-		// (set) Token: 0x0600001D RID: 29 RVA: 0x0000215B File Offset: 0x0000035B
-		public string InstrumentName { get; set; }
-
-		// Token: 0x17000008 RID: 8
-		// (get) Token: 0x0600001E RID: 30 RVA: 0x00002164 File Offset: 0x00000364
-		// (set) Token: 0x0600001F RID: 31 RVA: 0x0000216C File Offset: 0x0000036C
-		public string InstrumentDescription { get; set; }
-
-		// Token: 0x17000009 RID: 9
-		// (get) Token: 0x06000020 RID: 32 RVA: 0x00002175 File Offset: 0x00000375
-		// (set) Token: 0x06000021 RID: 33 RVA: 0x0000217D File Offset: 0x0000037D
-		public string InstrumentPortName { get; set; }
-
-		// Token: 0x1700000A RID: 10
-		// (get) Token: 0x06000022 RID: 34 RVA: 0x00002186 File Offset: 0x00000386
-		// (set) Token: 0x06000023 RID: 35 RVA: 0x0000218E File Offset: 0x0000038E
-		public string InstrumentBaudRate { get; set; }
-
-		// Token: 0x1700000B RID: 11
-		// (get) Token: 0x06000024 RID: 36 RVA: 0x00002197 File Offset: 0x00000397
-		// (set) Token: 0x06000025 RID: 37 RVA: 0x0000219F File Offset: 0x0000039F
-		public string InstrumentDataBits { get; set; }
-
-		// Token: 0x1700000C RID: 12
-		// (get) Token: 0x06000026 RID: 38 RVA: 0x000021A8 File Offset: 0x000003A8
-		// (set) Token: 0x06000027 RID: 39 RVA: 0x000021B0 File Offset: 0x000003B0
-		public long LastAnalysisId { get; set; }
-
-		// Token: 0x1700000D RID: 13
-		// (get) Token: 0x06000028 RID: 40 RVA: 0x000021B9 File Offset: 0x000003B9
-		// (set) Token: 0x06000029 RID: 41 RVA: 0x000021C1 File Offset: 0x000003C1
-		public StopBits? InstrumentStopBits { get; set; }
-
-		// Token: 0x1700000E RID: 14
-		// (get) Token: 0x0600002A RID: 42 RVA: 0x000021CA File Offset: 0x000003CA
-		// (set) Token: 0x0600002B RID: 43 RVA: 0x000021D2 File Offset: 0x000003D2
-		public Parity? InstrumentParity { get; set; }
-
-		// Token: 0x1700000F RID: 15
-		// (get) Token: 0x0600002C RID: 44 RVA: 0x000021DB File Offset: 0x000003DB
-		// (set) Token: 0x0600002D RID: 45 RVA: 0x000021E3 File Offset: 0x000003E3
-		public string InstrumentStd { get; set; }
-
-		// Token: 0x17000010 RID: 16
-		// (get) Token: 0x0600002E RID: 46 RVA: 0x000021EC File Offset: 0x000003EC
-		// (set) Token: 0x0600002F RID: 47 RVA: 0x000021F4 File Offset: 0x000003F4
-		public int? ServiceId { get; set; }
-
-		// Token: 0x17000011 RID: 17
-		// (get) Token: 0x06000030 RID: 48 RVA: 0x000021FD File Offset: 0x000003FD
-		// (set) Token: 0x06000031 RID: 49 RVA: 0x00002205 File Offset: 0x00000405
-		public int Mode { get; set; }
-
-		// Token: 0x17000012 RID: 18
-		// (get) Token: 0x06000032 RID: 50 RVA: 0x0000220E File Offset: 0x0000040E
-		// (set) Token: 0x06000033 RID: 51 RVA: 0x00002216 File Offset: 0x00000416
-		public long? LaboratoryUnitId { get; set; }
-
-		// Token: 0x17000013 RID: 19
-		// (get) Token: 0x06000034 RID: 52 RVA: 0x0000221F File Offset: 0x0000041F
-		// (set) Token: 0x06000035 RID: 53 RVA: 0x00002227 File Offset: 0x00000427
-		public int InstrumentCode { get; set; }
-
-		// Token: 0x17000014 RID: 20
-		// (get) Token: 0x06000036 RID: 54 RVA: 0x00002230 File Offset: 0x00000430
-		// (set) Token: 0x06000037 RID: 55 RVA: 0x00002238 File Offset: 0x00000438
-		public string MiddlewareServiceName { get; set; }
-	}
+        public long? L1 { get; set; }
+        public long? L2 { get; set; }
+        public long? L3 { get; set; }
+        public bool? B1 { get; set; }
+    }
 }
